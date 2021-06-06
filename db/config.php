ebc5ad7b72ae8211c326e61ed0d4aef3a92a4e6c
Praveen Suthaharan <praveen.suthaharan.github@gmail.com>
2021-06-06 19:09:18 -0400

    commented out alerts that were used for testing purposes

--- db/config.php
+++ db/config.php
@@ -30,7 +30,7 @@ if (file_exists($_SERVER["DOCUMENT_ROOT"] . '/config.php')) {
 
   if(isset($workerId)){
     // if running locally or on server...
-    echo '<script type="text/javascript">alert("workerId")</script>';
+    //echo '<script type="text/javascript">alert("workerId")</script>';
     // echo '<script type="text/javascript">let db_connection = false</script>';
     // echo '<script type="text/javascript">let turkprime_online = true</script>';
     $db_connection_status = false;
@@ -56,7 +56,7 @@ if (file_exists($_SERVER["DOCUMENT_ROOT"] . '/config.php')) {
  
   } else {
     // if running locally or on server...
-    echo '<script type="text/javascript">alert("hi")</script>';
+    //echo '<script type="text/javascript">alert("hi")</script>';
     // echo$db_connection_status;
     $db_connection_status = false;
     $turkprime_online = false;
@@ -80,14 +80,14 @@ if (file_exists($_SERVER["DOCUMENT_ROOT"] . '/config.php')) {
   $workerId = $_GET["workerId"];
   if(isset($workerId)){
     // if running locally with workerId...
-    echo '<script type="text/javascript">alert("workerIdisset")</script>';
+    //echo '<script type="text/javascript">alert("workerIdisset")</script>';
     echo '<script type="text/javascript">let db_connection = false</script>';
     echo '<script type="text/javascript">let turkprime_online = true</script>';
     $db_connection_status = false;
     $turkprime_online = true;
   } else {
       // if running locally...
-      echo '<script type="text/javascript">alert("workerIdisnotset")</script>';
+      //echo '<script type="text/javascript">alert("workerIdisnotset")</script>';
       $db_connection_status = false;
       $turkprime_online = false;
       echo '<script type="text/javascript">let db_connection = false</script>';
